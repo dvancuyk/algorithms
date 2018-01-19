ca54b4bf747f2e33ad3385b2386c10d2dc53d456
Derek <dvancuyk@gmail.com>
2018-01-18 19:35:51 -0500

    Implemented BFS

--- CSharp/Graphs.Tests/BreadthFirstSearchTests.cs
+++ CSharp/Graphs.Tests/BreadthFirstSearchTests.cs
@@ -1,11 +1,102 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Kayllian.Algorithms.Graphs;
+using FluentAssertions;
+using Xunit;
 
 namespace Graphs.Tests
 {
     public class BreadthFirstSearchTests
     {
+        [Fact]
+        public void PathToShould_MapCorrectPath_GivenPathExists()
+        {
+            // Act
+            const int origin = 0,
+                destination = 3;
+            var graph = GraphBuilder.Basic();
+            var search = new BreadthFirstSearch(graph, origin);
+            var expected = new[] { origin, 5, destination };
 
+            // Act
+            var paths = search.PathTo(destination);
+
+            // Assert
+            paths.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData(0, 4, 3)]
+        [InlineData(7, 8, 2)]
+        [InlineData(9, 12, 2)]
+        public void PathToShould_HaveShortestPath_GivenPathExists(int origin, int destination, int length)
+        {
+            // Act
+            var graph = GraphBuilder.Basic();
+            var search = new BreadthFirstSearch(graph, origin);
+
+            // Act
+            var paths = search.PathTo(destination);
+
+            // Assert
+            paths.Should().NotBeNull()
+                .And
+                .HaveCount(length);
+        }
+
+        [Theory]
+        [InlineData(0, 8)]
+        [InlineData(0, 9)]
+        [InlineData(7, 1)]
+        [InlineData(7, 12)]
+        [InlineData(9, 6)]
+        [InlineData(9, 8)]
+        public void PathToShould_ReturnNull_GivenNoPathExists(int origin, int destination)
+        {
+            // Act
+            var graph = GraphBuilder.Basic();
+            var search = new BreadthFirstSearch(graph, origin);
+
+            // Act
+            var paths = search.PathTo(destination);
+
+            // Assert
+            paths.Should().BeEmpty();
+        }
+
+        [Theory]
+        [InlineData(0, 4)]
+        [InlineData(7, 8)]
+        [InlineData(9, 12)]
+        public void HasShould_ReturnTrue_GivenPathExists(int origin, int destination)
+        {
+            // Act
+            var graph = GraphBuilder.Basic();
+            var search = new BreadthFirstSearch(graph, origin);
+
+            // Act
+            var paths = search.HasPathTo(destination);
+
+            // Assert
+            paths.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(0, 8)]
+        [InlineData(0, 9)]
+        [InlineData(7, 1)]
+        [InlineData(7, 12)]
+        [InlineData(9, 6)]
+        [InlineData(9, 8)]
+        public void HasShould_ReturnFalse_GivenNoPathExists(int origin, int destination)
+        {
+            // Act
+            var graph = GraphBuilder.Basic();
+            var search = new BreadthFirstSearch(graph, origin);
+
+            // Act
+            var paths = search.HasPathTo(destination);
+
+            // Assert
+            paths.Should().BeFalse();
+        }
     }
 }
---
+++ CSharp/Graphs/BreadthFirstSearch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kayllian.Algorithms.Graphs
+{
+    public class BreadthFirstSearch
+    {
+        private int origin;
+        private int[] edgeTo;
+        private bool[] marked;
+
+        public BreadthFirstSearch(Graph graph, int origin)
+        {
+            this.origin = origin;
+            edgeTo = new int[graph.Vertices];
+            marked = new bool[graph.Vertices];
+
+            Search(graph);
+        }
+
+        /// <summary>
+        /// Returns the path to the provided destination if it exists.
+        /// </summary>
+        /// <param name="destination"></param>
+        /// <returns>A collection of integers which represents the path from the origin to the provided destination if it exists; otherwise null is returned</returns>
+        public IEnumerable<int> PathTo(int destination)
+        {
+            var path = new Stack<int>();
+
+            if(HasPathTo(destination))
+            {
+                for(int current = destination; current != origin; current = edgeTo[current])
+                {
+                    path.Push(current);
+                }
+
+                path.Push(origin);
+            }
+
+            return path;
+        }
+
+        public bool HasPathTo(int destination)
+        {
+            return marked[destination];
+        }
+
+        private void Search(Graph graph)
+        {
+            var queue = new Queue<int>();
+            marked[origin] = true;
+
+            queue.Enqueue(origin);
+
+            while(queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                
+                foreach (var vertex in graph.Adjacent(current).Where(v => !marked[v]))
+                {
+                    marked[vertex] = true;
+                    edgeTo[vertex] = current;
+                    queue.Enqueue(vertex);
+                }
+            }
+            
+        }
+    }
+}
