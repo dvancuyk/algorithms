aeb604b41d19638eab86d4d7e9d17d6707ad4a15
Derek Van Cuyk <derek.vancuyk@agilethought.com>
2018-01-23 07:18:48 -0500

    Added DFS search for bipartite graphs

---
+++ CSharp/Graphs.Tests/TwoColorTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using Kayllian.Algorithms.Graphs;
+using Xunit;
+
+namespace Graphs.Tests
+{
+    public class TwoColorTests
+    {
+        [Fact]
+        public void IsBipartiteShould_ReturnFalse_GivenGraphIsNotBipartite()
+        {
+            // Arrange
+            var graph = GraphBuilder.Basic();
+            var test = new TwoColor(graph);
+
+            // Act
+            var isBipartite = test.IsBipartite;
+
+            // Assert
+            isBipartite.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsBipartiteShould_ReturnTrue_GivenGraphIsBipartite()
+        {
+            // Arrange
+            var graph = GraphBuilder.Bipartite();
+            var test = new TwoColor(graph);
+
+            // Act
+            var isBipartite = test.IsBipartite;
+
+            // Assert
+            isBipartite.Should().BeTrue();
+        }
+    }
+}
---
+++ CSharp/Graphs/TwoColor.cs
@@ -0,0 +1,48 @@
+﻿namespace Kayllian.Algorithms.Graphs
+{
+    /// <summary>
+    /// Determines if a graph is bipartite.
+    /// </summary>
+    public class TwoColor
+    {
+        private readonly bool[] _colors;
+        private readonly bool[] _marked;
+
+        public TwoColor(Graph graph)
+        {
+            IsBipartite = true;
+            _colors = new bool[graph.Vertices];
+            _marked = new bool[graph.Vertices];
+
+            for (int i = 0; i < graph.Vertices; i++)
+            {
+                if (!_marked[i])
+                {
+                    Scan(graph, i);
+                }
+            }
+        }
+
+        public bool IsBipartite { get; private set; }
+
+        private void Scan(Graph graph, int vertex)
+        {
+            _marked[vertex] = true;
+
+            foreach (var neighbor in graph.Adjacent(vertex))
+            {
+
+                if (!_marked[neighbor])
+                {
+                    _colors[neighbor] = !_colors[vertex];
+                    Scan(graph,  neighbor);
+                }
+                else if (_colors[neighbor] == _colors[vertex])
+                {
+                    IsBipartite = false;
+                }
+            }
+
+        }
+    }
+}
