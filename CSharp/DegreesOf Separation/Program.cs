8b90d0c4e76a52a9e3dadd79976e2e18212f627b
Derek <dvancuyk@gmail.com>
2018-01-25 21:08:14 -0500

    Added client implementing the degrees of separation

---
+++ CSharp/DegreesOf Separation/Program.cs
@@ -0,0 +1,54 @@
+﻿using Kayllian.Algorithms.Graphs;
+using System;
+using System.IO;
+
+namespace DegreesOf_Separation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = args[0];
+            var delimiter = args[1];
+            var symbol = args[2];
+
+            var graph = new SymbolGraph(File.ReadAllLines(fileName), delimiter);
+
+            if (!graph.Contains(symbol))
+            {
+                Console.WriteLine($"'{symbol}' cannot be found in the file '{fileName}'.");
+                return;
+            }
+
+            var symbolIndex = graph.Index(symbol);
+            var search = new BreadthFirstSearch(graph.Graph, symbolIndex);
+
+            string value = null;
+
+            do
+            {
+                value = Console.ReadLine();
+
+                if (!graph.Contains(value))
+                {
+                    Console.WriteLine($"'{value}' cannot be found in the file '{fileName}'.");
+                    continue;
+                }
+
+                var valueIndex = graph.Index(value);
+                if (!search.HasPathTo(valueIndex))
+                {
+                    Console.WriteLine($"There is no path from '{symbol}' to '{value}'.");
+                    continue;
+                }
+
+                foreach (var vertex in search.PathTo(valueIndex))
+                {
+                    Console.WriteLine($"\t{graph.Name(vertex)}");
+                }
+
+
+            } while (!string.IsNullOrEmpty(value));
+        }
+    }
+}
