e0dddfa80ae349bf7e689a56f527e5e5106fb79e
Derek <dvancuyk@gmail.com>
2018-01-28 19:48:08 -0500

    Added DirectedCycle search

---
+++ CSharp/Graphs.Tests/DirectedCycleTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Kayllian.Algorithms.Graphs;
+using Xunit;
+
+namespace Graphs.Tests
+{
+    public class DirectedCycleTests
+    {
+        [Fact]
+        public void HasCycleShould_ReturnTrue_GivenGraphHasCycle()
+        {
+            // Arrange
+            var graph = GraphBuilder.DefaultDirected();
+            var query = new DirectedCycle(graph);
+
+            // Act
+            var hasCycle = query.HasCycle;
+
+            // Assert
+            hasCycle.Should().BeTrue();
+        }
+
+        [Fact]
+        public void HasCycleShould_ReturnFalse_GivenGraphHasAcyclic()
+        {
+            // Arrange
+            var graph = new DirectedGraph(4);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(2, 3);
+
+            var query = new DirectedCycle(graph);
+
+            // Act
+            var hasCycle = query.HasCycle;
+
+            // Assert
+            hasCycle.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CycleShould_ReturnNonEmptyCollection_GivenGraphHasCycle()
+        {
+            // Arrange
+            var graph = GraphBuilder.DefaultDirected();
+            var query = new DirectedCycle(graph);
+
+            // Act
+            var cycle = query.Cycle();
+
+            // Assert
+            cycle.Should().NotBeNullOrEmpty();
+            
+
+        }
+    }
+}
---
+++ CSharp/Graphs/DirectedCycle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kayllian.Algorithms.Graphs
+{
+    /// <summary>
+    /// Determines if a graph contains a cycle.
+    /// </summary>
+    public class DirectedCycle
+    {
+        private bool[] _marked;
+        private bool[] _onStack;
+        private int[] _edgeTo;
+
+        private Stack<int> _cycle = new Stack<int>();
+
+        public DirectedCycle(IGraph graph)
+        {
+            _marked = new bool[graph.Vertices];
+            _onStack = new bool[graph.Vertices];
+            _edgeTo = new int[graph.Vertices];
+
+            for (int i = 0; i < graph.Vertices; i++)
+            {
+                if (_marked[i])
+                    continue;
+
+                Search(graph, i);
+            }
+        }
+
+        /// <summary>
+        /// Returns the determination whether or not the provided graph contains a cycle.
+        /// </summary>
+        public bool HasCycle => _cycle.Any();
+
+        /// <summary>
+        /// Returns back a cycle if one exists; otherwise an empty collection is returned.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<int> Cycle()
+        {
+            return _cycle;
+        }
+
+        private void Search(IGraph graph, int vertex)
+        {
+            _onStack[vertex] = true;
+            _marked[vertex] = true;
+
+            foreach (var neighbor in graph.Adjacent(vertex))
+            {
+                if (HasCycle)
+                    break;
+
+                _edgeTo[neighbor] = vertex;
+                if (_onStack[neighbor]) // we have a cycle
+                {
+                    _cycle = new Stack<int>();
+                    
+                    for (int i = vertex; i != neighbor; i = _edgeTo[i])
+                    {
+                        _cycle.Push(i);
+                    }
+
+                    _cycle.Push(neighbor);
+                    _cycle.Push(vertex);
+
+                }
+                else
+                    Search(graph, neighbor);
+            }
+
+            _onStack[vertex] = false;
+        }
+    }
+}
